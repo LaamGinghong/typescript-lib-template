cbf7a136e42a45026b2d444b86c39223abf8f077
LaamGinghong <351390560@qq.com>
2020-07-17 15:10:33 +0800

    update: webpack configuration

--- config/webpack.config.ts
+++ config/webpack.config.ts
@@ -2,14 +2,12 @@ import { BannerPlugin, Configuration } from 'webpack'
 import { resolve } from 'path'
 import WebpackBar from 'webpackbar'
 import FriendlyErrorsWebpackPlugin from 'friendly-errors-webpack-plugin'
-import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
 import HardSourceWebpackPlugin from 'hard-source-webpack-plugin'
 import { CleanWebpackPlugin } from 'clean-webpack-plugin'
 import SizePlugin from 'size-plugin'
 import TerserPlugin from 'terser-webpack-plugin'
 
 import { PROJECT_NAME, PROJECT_ROOT } from '../constants/env'
-import { tsWorkerPool } from '../constants/worker-pool'
 
 const configuration: Configuration = {
   devtool: 'source-map',
@@ -30,12 +28,9 @@ const configuration: Configuration = {
         test: /\.tsx?$/,
         exclude: /node_modules/,
         use: [
-          { loader: 'thread-loader', options: tsWorkerPool },
           {
             loader: 'ts-loader',
             options: {
-              transpileOnly: true,
-              happyPackMode: true,
               configFile: resolve(PROJECT_ROOT, 'tsconfig.json'),
               experimentalFileCaching: true,
             },
@@ -52,12 +47,6 @@ const configuration: Configuration = {
     }),
     new FriendlyErrorsWebpackPlugin(),
     new SizePlugin({ writeFile: false }),
-    new ForkTsCheckerWebpackPlugin({
-      typescript: {
-        memoryLimit: 4096,
-        configFile: resolve(PROJECT_ROOT, 'tsconfig.json'),
-      },
-    }),
     new CleanWebpackPlugin(),
     new HardSourceWebpackPlugin({ info: { mode: 'none', level: 'warn' } }),
   ],
